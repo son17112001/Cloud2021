fd050572de83c9f019b184bc55c60b7a718a6f75
Son <son17112001@gmail.com>
2021-11-28 22:56:11 +0700

    SRC

---
+++ src/common/constant/language.ts
@@ -0,0 +1,308 @@
+export interface ILanguage {
+  Language: string;
+  LanguageCode: string;
+}
+
+export const languge = [
+  { Language: "Auto Detected", LanguageCode: "auto" },
+  {
+    Language: "Afrikaans",
+    LanguageCode: "af",
+  },
+  {
+    Language: "Albanian",
+    LanguageCode: "sq",
+  },
+  {
+    Language: "Amharic",
+    LanguageCode: "am",
+  },
+  {
+    Language: "Arabic",
+    LanguageCode: "ar",
+  },
+  {
+    Language: "Armenian",
+    LanguageCode: "hy",
+  },
+  {
+    Language: "Azerbaijani",
+    LanguageCode: "az",
+  },
+  {
+    Language: "Bengali",
+    LanguageCode: "bn",
+  },
+  {
+    Language: "Bosnian",
+    LanguageCode: "bs",
+  },
+  {
+    Language: "Bulgarian",
+    LanguageCode: "bg",
+  },
+  {
+    Language: "Catalan",
+    LanguageCode: "ca",
+  },
+  {
+    Language: "Chinese (Simplified)",
+    LanguageCode: "zh",
+  },
+  {
+    Language: "Chinese (Traditional)",
+    LanguageCode: "zh-TW",
+  },
+  {
+    Language: "Croatian",
+    LanguageCode: "hr",
+  },
+  {
+    Language: "Czech",
+    LanguageCode: "cs",
+  },
+  {
+    Language: "Danish",
+    LanguageCode: "da",
+  },
+  {
+    Language: "Dari",
+    LanguageCode: "fa-AF",
+  },
+  {
+    Language: "Dutch",
+    LanguageCode: "nl",
+  },
+  {
+    Language: "English",
+    LanguageCode: "en",
+  },
+  {
+    Language: "Estonian",
+    LanguageCode: "et",
+  },
+  {
+    Language: "Farsi (Persian)",
+    LanguageCode: "fa",
+  },
+  {
+    Language: "Filipino, Tagalog",
+    LanguageCode: "tl",
+  },
+  {
+    Language: "Finnish",
+    LanguageCode: "fi",
+  },
+  {
+    Language: "French",
+    LanguageCode: "fr",
+  },
+  {
+    Language: "French (Canada)",
+    LanguageCode: "fr-CA",
+  },
+  {
+    Language: "Georgian",
+    LanguageCode: "ka",
+  },
+  {
+    Language: "German",
+    LanguageCode: "de",
+  },
+  {
+    Language: "Greek",
+    LanguageCode: "el",
+  },
+  {
+    Language: "Gujarati",
+    LanguageCode: "gu",
+  },
+  {
+    Language: "Haitian Creole",
+    LanguageCode: "ht",
+  },
+  {
+    Language: "Hausa",
+    LanguageCode: "ha",
+  },
+  {
+    Language: "Hebrew",
+    LanguageCode: "he",
+  },
+  {
+    Language: "Hindi",
+    LanguageCode: "hi",
+  },
+  {
+    Language: "Hungarian",
+    LanguageCode: "hu",
+  },
+  {
+    Language: "Icelandic",
+    LanguageCode: "is",
+  },
+  {
+    Language: "Indonesian",
+    LanguageCode: "id",
+  },
+  {
+    Language: "Irish",
+    LanguageCode: "ga",
+  },
+  {
+    Language: "Italian",
+    LanguageCode: "it",
+  },
+  {
+    Language: "Japanese",
+    LanguageCode: "ja",
+  },
+  {
+    Language: "Kannada",
+    LanguageCode: "kn",
+  },
+  {
+    Language: "Kazakh",
+    LanguageCode: "kk",
+  },
+  {
+    Language: "Korean",
+    LanguageCode: "ko",
+  },
+  {
+    Language: "Latvian",
+    LanguageCode: "lv",
+  },
+  {
+    Language: "Lithuanian",
+    LanguageCode: "lt",
+  },
+  {
+    Language: "Macedonian",
+    LanguageCode: "mk",
+  },
+  {
+    Language: "Malay",
+    LanguageCode: "ms",
+  },
+  {
+    Language: "Malayalam",
+    LanguageCode: "ml",
+  },
+  {
+    Language: "Maltese",
+    LanguageCode: "mt",
+  },
+  {
+    Language: "Marathi",
+    LanguageCode: "mr",
+  },
+  {
+    Language: "Mongolian",
+    LanguageCode: "mn",
+  },
+  {
+    Language: "Norwegian",
+    LanguageCode: "no",
+  },
+  {
+    Language: "Pashto",
+    LanguageCode: "ps",
+  },
+  {
+    Language: "Polish",
+    LanguageCode: "pl",
+  },
+  {
+    Language: "Portuguese",
+    LanguageCode: "pt",
+  },
+  {
+    Language: "Portuguese (Portugal)",
+    LanguageCode: "pt-PT",
+  },
+  {
+    Language: "Punjabi",
+    LanguageCode: "pa",
+  },
+  {
+    Language: "Romanian",
+    LanguageCode: "ro",
+  },
+  {
+    Language: "Russian",
+    LanguageCode: "ru",
+  },
+  {
+    Language: "Serbian",
+    LanguageCode: "sr",
+  },
+  {
+    Language: "Sinhala",
+    LanguageCode: "si",
+  },
+  {
+    Language: "Slovak",
+    LanguageCode: "sk",
+  },
+  {
+    Language: "Slovenian",
+    LanguageCode: "sl",
+  },
+  {
+    Language: "Somali",
+    LanguageCode: "so",
+  },
+  {
+    Language: "Spanish",
+    LanguageCode: "es",
+  },
+  {
+    Language: "Spanish (Mexico)",
+    LanguageCode: "es-MX",
+  },
+  {
+    Language: "Swahili",
+    LanguageCode: "sw",
+  },
+  {
+    Language: "Swedish",
+    LanguageCode: "sv",
+  },
+  {
+    Language: "Tamil",
+    LanguageCode: "ta",
+  },
+  {
+    Language: "Telugu",
+    LanguageCode: "te",
+  },
+  {
+    Language: "Thai",
+    LanguageCode: "th",
+  },
+  {
+    Language: "Turkish",
+    LanguageCode: "tr",
+  },
+  {
+    Language: "Ukrainian",
+    LanguageCode: "uk",
+  },
+  {
+    Language: "Urdu",
+    LanguageCode: "ur",
+  },
+  {
+    Language: "Uzbek",
+    LanguageCode: "uz",
+  },
+  {
+    Language: "Vietnamese",
+    LanguageCode: "vi",
+  },
+  {
+    Language: "Welsh",
+    LanguageCode: "cy",
+  },
+];
