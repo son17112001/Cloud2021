4b01b04cc067cc6aaa867beeb5c5e878bfa6884e
Son <son17112001@gmail.com>
2021-11-28 22:38:08 +0700

    INIT PROJECT

---
+++ next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+
+    return config;
+  },
+};
