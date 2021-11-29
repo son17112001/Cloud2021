68573afb5646649f09482a2703f4dfbc5502acaf
phatphamm01 <84543546+phatphamm01@users.noreply.github.com>
2021-11-30 00:24:35 +0700

    add hook - useToggleAndCloseVer2

---
+++ src/hooks/useToggleAndCloseVer2/index.tsx
@@ -0,0 +1,35 @@
+import { RefObject, useCallback, useEffect, useState } from "react";
+
+const useToggleAndCloseVer2 = (
+  ref: RefObject<HTMLElement>,
+  initialState: boolean = false
+): [boolean, any] => {
+  const [state, setState] = useState<boolean>(initialState);
+
+  const handleClose = useCallback((e: MouseEvent) => {
+    const element: HTMLDivElement | null = e?.target as HTMLDivElement;
+
+    if (ref && ref.current) {
+      const isFound = !ref.current.contains(element);
+
+      if (isFound) {
+        setState(false);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    let { current } = ref;
+
+    if (current) {
+      document.addEventListener("mousedown", handleClose, false);
+    } else {
+      document.removeEventListener("mousedown", handleClose, false);
+    }
+  }, [state]);
+
+  // const toggle = useCallback((): void => setState((state) => !state), []);
+  return [state, setState];
+};
+
+export default useToggleAndCloseVer2;
