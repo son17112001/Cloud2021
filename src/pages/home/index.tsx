c7cf9ec83ee34d72175d7a9327fd9769aefe9869
Ng. Hoang Vu <vu2872001@gmail.com>
2021-12-07 00:12:11 +0700

    add pages

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,16 +3,6 @@ import { Fragment } from "react";
 import Home from "@containers/Home";
 import MetaTitle from "@design/MetaTitle";
 
-// import { ILanguage } from "@common/constant/language";
-// import ChangeLanguage from "@components/ChangeLanguage";
-// import Input from "@components/Input";
-// import Header from "@components/Layout/Header";
-// import AWSTranslate from "@services/AWS";
-// import { ITranslateTextPayload } from "@services/AWS/interface";
-// import { useRouter } from "next/dist/client/router";
-// import { ChangeEvent, FC, useEffect, useRef, useState } from "react";
-// import tw, { styled } from "twin.macro";
-
 const Homepage: NextPage = () => {
   return (
     <Fragment>
